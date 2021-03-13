531a30cfdea171541314c701e16e88a0dd66b525
Nicky Bulthuis <github@mailalias.nl>
2021-03-13 16:02:05 +0100

    Quote app.

---
+++ app/quotes/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from django.utils.translation import gettext as _
+
+from .models import Security, Quote, Scraper
+
+
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+
+    def view_price(self, obj):
+        return '€ {0:.4f}'.format(obj.price)
+
+    view_price.short_description = _('Price')
+
+    date_hierarchy = 'date'
+    list_display = ('date', 'security', 'view_price')
+    list_filter = ('security',)
+
+
+@admin.register(Security)
+class SecurityAdmin(admin.ModelAdmin):
+
+    def view_last_price(self, obj):
+        return '€ {0:.4f}'.format(obj.quotes.latest().price)
+
+    view_last_price.short_description = _('Latest Price')
+
+    def view_last_date(self, obj):
+        return '{0}'.format(obj.quotes.latest().date)
+
+    view_last_date.short_description = _('Latest Date')
+
+    list_display = ('name', 'view_last_date', 'view_last_price',)
+    search_fields = ['name', ]
+
+
+@admin.register(Scraper)
+class ScraperAdmin(admin.ModelAdmin):
+    list_display = ('name', 'security', 'url',)
+    search_fields = ['name', ]
---
+++ app/quotes/tests.py
@@ -0,0 +1,106 @@
+import responses
+from django.test import TransactionTestCase
+
+from .models import Scraper, Security
+
+
+class ScraperTestCase(TransactionTestCase):
+
+    def setUp(self):
+        self.security = Security.objects.create(name='Security')
+
+    @responses.activate
+    def test_update_quotes_not_found_0_quotes(self):
+        scraper = Scraper.objects.create(name="Scraper", security=self.security,
+                                         url="https://quote_url", date_key='x', price_key='y')
+
+        responses.add(responses.GET, 'https://quote_url',
+                      json={'not found': '404'}, status=404)
+        scraper.update_quotes()
+
+        self.assertNumQueries(0)
+        self.assertEqual(self.security.quotes.count(), 0)
+
+    @responses.activate
+    def test_update_quotes_json_creates_3_quotes(self):
+        scraper = Scraper.objects.create(name="Scraper", security=self.security,
+                                         url="https://quote_url", date_key='x', price_key='y')
+        data = [{"x": '2021-01-01', "y": 10}, {"x": '2021-01-02', "y": 10.5}, {"x": '2021-01-03', "y": 11}]
+
+        responses.add(responses.GET, 'https://quote_url',
+                      json=data, status=200)
+        scraper.update_quotes()
+
+        self.assertNumQueries(3)
+        self.assertEqual(self.security.quotes.count(), 3)
+
+
+    @responses.activate
+    def test_update_quotes_invalid_date_key_raises_exception(self):
+        scraper = Scraper.objects.create(name="Scraper", security=self.security,
+                                         url="https://quote_url", date_key='not_present', price_key='y')
+        data = [{"x": '2021-01-01', "y": 10}]
+
+        responses.add(responses.GET, 'https://quote_url',
+                      json=data, status=200)
+
+        with self.assertRaises(KeyError):
+            scraper.update_quotes()
+
+        self.assertNumQueries(0)
+        self.assertEqual(self.security.quotes.count(), 0)
+
+
+    @responses.activate
+    def test_update_quotes_invalid_date_key_raises_exception(self):
+        scraper = Scraper.objects.create(name="Scraper", security=self.security,
+                                         url="https://quote_url", date_key='x', price_key='not_present')
+        data = [{"x": '2021-01-01', "y": 10}]
+
+        responses.add(responses.GET, 'https://quote_url',
+                      json=data, status=200)
+
+        with self.assertRaises(KeyError):
+            scraper.update_quotes()
+
+        self.assertNumQueries(0)
+        self.assertEqual(self.security.quotes.count(), 0)
+
+    @responses.activate
+    def test_update_quotes_matching_regex_creates_3_quotes(self):
+        scraper = Scraper.objects.create(name="Scraper", security=self.security,
+                                         url="https://quote_url", date_key='x', price_key='y',
+                                         regex="data:\s(\[{.*}+\])")
+
+        body = "data: [{\"x\":\"2021-01-01\",\"y\":10}," \
+               "{\"x\":\"2021-01-02\",\"y\":10.50}," \
+               "{\"x\":\"2021-01-03\",\"y\":11}]"
+
+        responses.add(responses.GET, 'https://quote_url',
+                      body=body, status=200)
+
+        scraper.update_quotes()
+
+        self.assertNumQueries(3)
+        self.assertEqual(self.security.quotes.count(), 3)
+
+    @responses.activate
+    def test_update_quotes_creates_only_new_quotes(self):
+        data = [{"x": '2021-01-02', "y": 10}]
+        responses.add(responses.GET, 'https://quote_url',
+                      json=data, status=200)
+        data = [{"x": '2021-01-01', "y": 10}, {"x": '2021-01-02', "y": 10.5}, {"x": '2021-01-03', "y": 11}]
+        responses.add(responses.GET, 'https://quote_url',
+                      json=data, status=200)
+
+        scraper = Scraper.objects.create(name="Scraper", security=self.security,
+                                         url="https://quote_url", date_key='x', price_key='y')
+
+        scraper.update_quotes()
+        self.assertEqual(self.security.quotes.count(), 1)
+
+        scraper.update_quotes()
+        self.assertEqual(self.security.quotes.count(), 2)
+
+        self.assertQuerysetEqual(self.security.quotes.all(), ['<Quote: Security (2021-01-02) = 10.0000>',
+                                                              '<Quote: Security (2021-01-03) = 11.0000>'])
